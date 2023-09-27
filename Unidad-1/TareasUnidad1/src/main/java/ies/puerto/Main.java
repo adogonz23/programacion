b680b679268cc97274ad16422f3a2d1a3a879a06
adogonz23 <145137932+adogonz23@users.noreply.github.com>
2023-09-27 13:51:16 +0200

    Update Main.java

--- Unidad-1/TareasUnidad1/src/main/java/ies/puerto/Main.java
+++ Unidad-1/TareasUnidad1/src/main/java/ies/puerto/Main.java
@@ -4,24 +4,36 @@ import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Ejercicio2 {
+public class MayorMenor {
+
     public static void main(String[] args) {
-        int valorA;
-        int valorB;
-        int valorC;
+        Scanner scanner = new Scanner(System.in);
 
-        Scanner lectura= new Scanner(System.in);
-        //BLOQUE 1: PEDIR VALOR DE VARIABLES Y ASIGNAR VALOR MAS A LAS VARIABLES ( EN CASO DE SER IGUALES VOLVER A INICIO)
+        int A, B, C;
+
         do {
-            System.out.println("Ingresa el valorde A:");
-            valorA = lectura.nextInt();
-            System.out.println("Ingresa el valorde B:");
-            valorB = lectura.nextInt();
-            System.out.println("Ingresa el valorde C:");
-            valorC = lectura.nextInt();
-            if (
-        };
+            System.out.print("Introduce el valor de A: ");
+            A = scanner.nextInt();
 
+            System.out.print("Introduce el valor de B: ");
+            B = scanner.nextInt();
 
+            System.out.print("Introduce el valor de C: ");
+            C = scanner.nextInt();
+
+            if (A == B || A == C || B == C) {
+                System.out.println("Error: Los valores deben ser distintos. Inténtelo de nuevo.");
+            } else {
+                break; // Si los valores son distintos, salimos del bucle
+            }
+        } while (true);
+
+        // Calcular el mayor y el menor
+        int mayor = Math.max(Math.max(A, B), C);
+        int menor = Math.min(Math.min(A, B), C);
+
+        // Imprimir los resultados
+        System.out.println("El mayor es: " + mayor);
+        System.out.println("El menor es: " + menor);
     }
 }
