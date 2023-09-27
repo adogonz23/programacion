59c69d95f78d5a59cd0429ebd8aa7f49b03dc956
adogonz23 <adogonzalez75@gmail.com>
2023-09-27 15:34:37 +0100

    actualizacion

--- Unidad-1/TareasUnidad1/src/main/java/ies/puerto/Main.java
+++ Unidad-1/TareasUnidad1/src/main/java/ies/puerto/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class MayorMenor {
 
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
