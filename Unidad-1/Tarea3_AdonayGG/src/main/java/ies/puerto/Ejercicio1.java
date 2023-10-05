1f6632a174fa995c1e1f54ea9561f1b8ac83ceb2
adogonz23 <adogonzalez75@gmail.com>
2023-10-05 16:27:33 +0100

    tarea3

---
+++ Unidad-1/Tarea3_AdonayGG/src/main/java/ies/puerto/Ejercicio1.java
@@ -0,0 +1,14 @@
+package ies.puerto;
+
+public class Ejercicio1 {
+    public static void main (String[] args){
+        int n;
+        for (n=1; n <=10; n++){
+            System.out.println(n);
+        }
+
+
+
+    }
+}
+
