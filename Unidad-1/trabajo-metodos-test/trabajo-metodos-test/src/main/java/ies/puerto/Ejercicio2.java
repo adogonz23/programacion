d868553307a03ca6a0935d4037b58f29ffba2353
adogonz23 <adogonzalez75@gmail.com>
2023-10-19 14:39:34 +0100

    añadimos nueva carpeta test

---
+++ Unidad-1/trabajo-metodos-test/trabajo-metodos-test/src/main/java/ies/puerto/Ejercicio2.java
@@ -0,0 +1,16 @@
+package ies.puerto;
+
+/**
+ * Clase que tiene como objetivo realizar
+ * @author
+ */
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void metodo() {
+
+    }
+
+}
