b00d1b2c338b48aa76e914b629f3c1515c95247f
adogonz23 <adogonzalez75@gmail.com>
2024-03-06 17:47:23 +0000

    se continua con los ejercicios

--- 3ºtrimestre/tareas/tareaficheros/src/main/java/ies/puerto/negocio/implementacion/TiendaNegocio.java
+++ 3ºtrimestre/tareas/tareaficheros/src/main/java/ies/puerto/negocio/implementacion/TiendaNegocio.java
@@ -1,5 +1,8 @@
 package ies.puerto.negocio.implementacion;
 
+import java.util.List;
+
+import ies.puerto.modelo.entity.Alimento;
 import ies.puerto.modelo.file.implementacion.csv.FileCsv;
 
 public class TiendaNegocio extends FileCsv{
@@ -7,5 +10,45 @@ public class TiendaNegocio extends FileCsv{
     
     public TiendaNegocio(){
         fileCsv =new FileCsv();
+    }
+    public List<Alimento> obtenerListaAlimentos(){
+        return fileCsv.obtenerAlimentos();
+    }
+    public Alimento mostrarAlimento(int id){
+        Alimento alimento = new Alimento(id);
+        List<Alimento> alimentos= obtenerAlimentos();
+        if (alimentos.contains(alimento)) {
+            return alimentos.get(alimentos.indexOf(alimento));
+        }
+        alimento=null;
+        return alimento;
+    }
+    public boolean actualizarAlimento(Alimento alimento){
+        List<Alimento> alimentos= obtenerListaAlimentos();
+        if (existeAlimento(alimentos, alimento)) {
+            alimentos.set(alimentos.indexOf(alimento), alimento);
+            return true;
+        }
+        return false;
+    }
+    public boolean notEmpty(List<Alimento> alimentos){
+        if (alimentos.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+    public boolean existeAlimento(List<Alimento>alimentos,Alimento alimento){
+        if (alimentos.contains(alimento)) {
+            return true;
+        }
+        return false;
+    }
+    public boolean eliminarAlimento(int id, List<Alimento>alimentos){
+        Alimento alimento=new Alimento(id);
+        if (alimentos.contains(alimento)) {
+            return alimentos.remove(alimento);
+            
+        }
+        return true;
     }
 }
