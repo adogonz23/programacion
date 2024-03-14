bbf0eac4379f83e212f341c4cd684befbcbb602d
adogonz23 <adogonzalez75@gmail.com>
2024-03-14 12:11:13 +0000

    a

--- 3ºtrimestre/retosPro/reto6/src/main/java/ies/puerto/App.java
+++ 3ºtrimestre/retosPro/reto6/src/main/java/ies/puerto/App.java
@@ -1,14 +1,24 @@
 package ies.puerto;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        
+
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public static void main( String[] args ){
+        char numero = 9;
+        double resultado =  Math.pow(numero, 23);
+        System.out.println(Math.min(100, 10));
+    }
+    public static String nocomunes(String palabra, String palabra2){
+        String salida="";
+
+        for (char caracter : palabra.toLowerCase().toCharArray()) {
+            if (palabra2.indexOf(caracter)==-1 && salida.indexOf(String.valueOf(caracter))==+1) {
+                salida+=caracter;
+            }
+        }
+        return salida;
     }
     
 }
