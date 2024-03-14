fcdc9db3296b3c5924cc8f0f6245349c0584bae0
adogonz23 <adogonzalez75@gmail.com>
2024-03-14 22:38:43 +0000

    seguimos practicando

--- 3ºtrimestre/retosPro/reto6/src/main/java/ies/puerto/App.java
+++ 3ºtrimestre/retosPro/reto6/src/main/java/ies/puerto/App.java
@@ -1,24 +1,25 @@
 package ies.puerto;
 
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 
 public class App {
     public static void main( String[] args ){
-        char numero = 9;
-        double resultado =  Math.pow(numero, 23);
-        System.out.println(Math.min(100, 10));
-    }
-    public static String nocomunes(String palabra, String palabra2){
-        String salida="";
+        
+        String fechaHoraString = "03-12-2012"; // Ejemplo de String de fecha y hora
+        
+        // Define el formato del String
+        DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
-        for (char caracter : palabra.toLowerCase().toCharArray()) {
-            if (palabra2.indexOf(caracter)==-1 && salida.indexOf(String.valueOf(caracter))==+1) {
-                salida+=caracter;
-            }
-        }
-        return salida;
+        // Parsea el String a LocalDateTime
+        LocalDate fechaHora = LocalDate.parse(fechaHoraString, formateador);
+
+        // Imprime el LocalDateTime
+        System.out.println("Fecha y hora parseadas: " + fechaHora);
     }
     
 }
