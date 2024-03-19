68e822be72052363072aae6b6652fa15f9ffba9c
adogonz23 <adogonzalez75@gmail.com>
2024-03-19 16:19:00 +0000

    a

--- 3ºtrimestre/tareas/tareaMarverFicheros/src/main/java/ies/puerto/modelo/utilidades/Utilidades.java
+++ 3ºtrimestre/tareas/tareaMarverFicheros/src/main/java/ies/puerto/modelo/utilidades/Utilidades.java
@@ -2,12 +2,7 @@ package ies.puerto.modelo.utilidades;
 
 public class Utilidades {
 
-    public final String DELIMITADOR =",";
+    public final static String DELIMITADOR =",";
 
-    public Utilidades() {
-    }
 
-    public String getDELIMITADOR() {
-        return DELIMITADOR;
-    }
 }
